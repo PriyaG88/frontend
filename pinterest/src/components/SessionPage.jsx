f8c00fc27691fe779cdfdd98b7a7543f0f1a630a
PriyaG88 <priya.g71488@gmail.com>
2018-04-14 23:49:52 -0700

    Add folder

---
+++ pinterest/src/components/SessionPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SessionForm from './SessionForm';
+
+const SessionPage = () => {
+  return (
+    <div className='session-page-container'>
+      <SessionForm />
+      <div className='session-info-container'>
+        <p>Pinterest helps you find ideas to try.</p>
+        <button type='button' className='info-btn'>How it works</button>
+      </div>
+    </div>
+  );
+};
+
+export default SessionPage;
