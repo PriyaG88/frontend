d5fbd4bf1ecf6bbd6a910930ccbaf7460ea286f7
PriyaG88 <priya.g71488@gmail.com>
2018-04-14 23:51:08 -0700

    Rename class name on container div and update component name being imported

--- pinterest/src/App.js
+++ pinterest/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Session from './components/session';
+import SessionPage from './components/SessionPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Session />
+      <div className='session-background'>
+        <SessionPage />
       </div>
     );
   }
