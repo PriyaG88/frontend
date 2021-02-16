7f8c0a2ed48c73852d397f7f79bb966cdc186e39
priyag88 <priyaprograms@gmail.com>
2021-02-15 16:18:30 -0800

    Wrap initialization/event listeners in an IIFE to avoid polluting global space Add error handling by displaying error text when user tries to submit an empty To Do item

--- todo/script.js
+++ todo/script.js
@@ -1,3 +1,38 @@
-const button = document.querySelector("#add-btn");
-const todoList = document.querySelector("#todo-list");
+(() => {
+    const button = document.querySelector("#add-btn");
+    const inputField = document.querySelector("#input-field");
+    const todoList = document.querySelector("#todo-list");
+    const errorContainer = document.querySelector("#error-container");
 
+    button.addEventListener("click", () => {
+        let text = inputField.value;
+        let li = document.createElement("li");
+        let textNode = document.createTextNode(text);
+        let closeBtn = document.createElement("span");
+
+        if (text.length === 0) {
+            showError(errorContainer);
+        } else {
+            hideError(errorContainer);
+            
+            closeBtn.appendChild(document.createTextNode("\u00D7"));
+            closeBtn.classList.add("close-btn");
+            li.appendChild(textNode);
+            li.appendChild(closeBtn);
+            todoList.appendChild(li);
+            inputField.value = "";
+
+            closeBtn.addEventListener("click", e => {
+                e.target.parentElement.remove();
+            });
+        }
+    });
+})();
+
+const showError = error => {
+    error.style.display = "block";
+}
+
+const hideError = error => {
+    error.style.display = "none";
+}
