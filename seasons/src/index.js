da9307e6002a16d5d0c60ea825652f4909449dec
priyag88 <priyaprograms@gmail.com>
2021-08-29 16:52:49 -0700

    Remove default src files Add semantic ui Add base Season Display component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  let latitude = undefined;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => (latitude = position.coords.latitude),
+    (error) => console.log(error)
+  );
+  return <div>{latitude}</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
