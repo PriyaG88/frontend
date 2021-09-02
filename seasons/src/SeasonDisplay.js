2e51a77d3d30cc8d4435c2ff4f2c7f3a7cac61f1
priyag88 <priyaprograms@gmail.com>
2021-09-01 21:00:37 -0700

    - Add getSeason helper function that returns season based on user's geolocation - Update SeasonDisplay component to display text based on user's geolocation based season

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) return lat > 0 ? "Summer" : "Winter";
+  else return lat > 0 ? "Winter" : "Summer";
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.latitutde, new Date().getMonth());
+  const text =
+    season === "Winter" ? "Brr, it is chilly!" : "Lets hit the beach!";
+
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   state = {
@@ -21,7 +22,7 @@ class App extends React.Component {
       return this.state.errorMessage ? (
         <div>{this.state.errorMessage}</div>
       ) : (
-        <div>Latitutde: {this.state.latitude}</div>
+        <SeasonDisplay latitude={this.state.latitude} />
       );
     }
   }
