5dea2d94cff2a1b634e2d2be23b810d339eef464
priyag88 <priyaprograms@gmail.com>
2021-09-01 22:00:53 -0700

    - Add conditional check to set text and icon values

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,18 +1,27 @@
 import React from "react";
 
 const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) return lat > 0 ? "Summer" : "Winter";
-  else return lat > 0 ? "Winter" : "Summer";
+  if (month > 2 && month < 9) return lat > 0 ? "summer" : "winter";
+  else return lat > 0 ? "winter" : "summer";
 };
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.latitutde, new Date().getMonth());
-  const text =
-    season === "Winter" ? "Brr, it is chilly!" : "Lets hit the beach!";
+  let text;
+  let icon;
+  if (season === "winter") {
+    text = "Brr, it is chilly!";
+    icon = "snowflake";
+  } else {
+    text = "Lets hit the beach!";
+    icon = "sun";
+  }
 
   return (
     <div>
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
